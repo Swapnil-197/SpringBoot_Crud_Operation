7d39736d0bfe50f5219ec07f180203d5f201c205
Swapnil-197 <sspatil1907@gmail.com>
2024-07-23 19:22:24 +0530

    POST and GET method added

--- SpringBoot_Crud_Application-1/src/main/java/com/example/demo/service/EmployeeServiceImple.java
+++ SpringBoot_Crud_Application-1/src/main/java/com/example/demo/service/EmployeeServiceImple.java
@@ -1,8 +1,33 @@
 package com.example.demo.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeServiceImple implements EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+	
+	@Override
+	public String addEmployee(Employee employee) {
+		repository.save(employee);
+		return "Success";
+	}
+
+	@Override
+	public Employee getEmployee(Integer id) {
+		 Optional<Employee> byId = repository.findById(id);
+		 if(byId.isPresent()) {
+			 return byId.get();
+		 }
+		
+		 return null;
+	}
 
 }
